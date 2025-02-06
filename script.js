4d187e1f9344ab908f0fb09c0a9cab933312353e
AD0808 <108204118+AD0808@users.noreply.github.com>
2025-02-06 11:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function firstWord(s) {
 
-console.log(s.split(" ")[0]);
+console.log(s.trim().split(" ")[0];
 }
+
+
+
 
 // Do not change the code below
 
-// const s = prompt("Enter String:");
-// // alert(firstWord(s));
+const s = prompt("Enter String:");
+alert(firstWord(s));
