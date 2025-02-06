32a97b8e65e98215ba7238ca41fbf3f5d7e4634c
AD0808 <108204118+AD0808@users.noreply.github.com>
2025-02-06 11:42:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function firstWord(s) {
-  // your code here
-}
+ let spaceIndex = s.indexOf(" "); // Find the first space
+  return spaceIndex === -1 ? s : s.substring(0, spaceIndex);}
 
 // Do not change the code below
 
